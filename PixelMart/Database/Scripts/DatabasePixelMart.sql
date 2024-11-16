7c9e39125e39ab0585dcb0e9d07346b54dedbfdc
ThiagoVenturini <thethiagoventurini@gmail.com>
2024-11-16 12:40:19 -0300

    alterações banco de dados

--- PixelMart/Database/Scripts/DatabasePixelMart.sql
+++ PixelMart/Database/Scripts/DatabasePixelMart.sql
@@ -15,7 +15,6 @@ drop table contas
 CREATE TABLE contas (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    genero VARCHAR(10) not null,
     cpf CHAR(20) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(100) NOT NULL
@@ -30,11 +29,33 @@ CREATE TABLE logins (
 );
 
 
+CREATE TABLE Pagamentos (
+    id Serial PRIMARY KEY,
+    nome_no_cartao VARCHAR(100) NOT NULL,
+    numero_do_cartao VARCHAR(20) NOT NULL,
+    data_validade CHAR(30) NOT NULL, 
+    cvv CHAR(5) NOT NULL
+);
+
+select * from pagamentos p 
+
+
 SELECT current_user;
 
 SELECT user;
 
-select * from contas c 
+select * from contas c
+
+select * from logins l 
+
+drop table pagamentos 
+
+
+ALTER TABLE contas
+ALTER COLUMN email TYPE VARCHAR(255);  -- Aumenta o limite do email
+ALTER COLUMN nome TYPE VARCHAR(255);   -- Aumenta o limite do nome
+ALTER COLUMN senha TYPE VARCHAR(255);  -- Aumenta o limite da senha
+
 
 ALTER TABLE contas
 ADD CONSTRAINT contas_genero_check
