2e64f398e4fbc2c0bfed7788082dbc9bdded568f
Thiago Scaravonatto <104689440+thiagosctto@users.noreply.github.com>
2024-11-19 19:48:27 -0300

    Update cartao_pagamento.sql

--- cartao_pagamento.sql
+++ cartao_pagamento.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Pagamentos (
     id Serial PRIMARY KEY,
     nome_no_cartao VARCHAR(100) NOT NULL,
-    numero_do_cartao VARCHAR(20) NOT NULL,
+    numero_do_cartao INTEGER(16) NOT NULL,
     data_validade CHAR(7) NOT NULL, 
-    cvv CHAR(4) NOT NULL
+    cvv INTEGER(4) NOT NULL
 );
